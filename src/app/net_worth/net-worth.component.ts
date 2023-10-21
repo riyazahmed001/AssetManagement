b89819cda34659e18ec08a3ea21cb1284e76d54f
riyazahmed001 <rafiriyazahmed@gmail.com>
2023-10-21 13:29:30 +0530

    networth started

--- src/app/net_worth/net-worth.component.ts
+++ src/app/net_worth/net-worth.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../services/util.service';
 import netWorthData from 'G:/Other computers/My Laptop/Statements/Projects/AssetManagement/constants/netWorthDetails.json';
 import { NetworthData } from 'src/app/models/NetworthData';
-
+import { NetworthYearlyData } from 'src/app/models/NetworthYearlyData';
 @Component({
   selector: 'app-net-worth',
   templateUrl: './net-worth.component.html',
@@ -9,10 +10,25 @@ import { NetworthData } from 'src/app/models/NetworthData';
 })
 export class NetWorthComponent implements OnInit {
   data: NetworthData;
+  currentNetworth: number = 0;
 
-  constructor() {
+  constructor(private utilService: UtilService) {
     this.data = netWorthData;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentNetworth = this.utilService.getCurrencyFormat(this.getCurrentNetworth());
+  }
+
+  public getCurrentNetworth(): number {
+    const result = this.data.data.reduce((accumulator:number, currentYear: NetworthYearlyData)=> {
+      accumulator = currentYear.values.reduce((total:number, value:string) => {
+        total += parseInt(value);
+        return total;
+      },0);
+      return accumulator;
+    },0);
+
+    return result;
+  }
 }
